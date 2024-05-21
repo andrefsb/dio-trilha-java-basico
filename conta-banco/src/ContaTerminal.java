760446a697fb24d5a8d9542e37e34bf5fac9c4c3
André Sampaio <andresampaio@id.uff.br>
2024-05-21 15:14:17 -0300

    Recebimento de inputs e tratamento de erro.

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,33 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.println("Por favor, digite o número da Conta:");
+            int numero = scanner.nextInt();
+
+            scanner.nextLine();
+
+            System.out.println("Por favor, digite o número da Agência:");
+            String agencia = scanner.nextLine();
+
+            System.out.println("Por favor, digite o nome do Cliente:");
+            String nomeCliente = scanner.nextLine();
+
+            System.out.println("Por favor, digite o saldo:");
+            double saldo = scanner.nextDouble();
+
+            String mensagem = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, " +
+                              "sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + 
+                              " já está disponível para saque.";
+            System.out.println(mensagem);
+        } catch (InputMismatchException e) {
+            System.out.println("Erro: Entrada inválida. Por favor, insira os dados corretamente.");
+        } finally {
+            scanner.close();
+        }
     }
 }
